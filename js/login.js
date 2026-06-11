d74f7315bbba70492d6bd8ab98e386f99abe98af
Pedro Henrique Rodrigues da Silva <pedrohrs@motorola.com>
2026-06-11 17:55:35 -0300

    javascript de login

---
+++ js/login.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', async function (event) {
+        
+        event.preventDefault();
+
+        const emailInput = document.getElementById('email').value.trim();
+        const passwordInput = document.getElementById('password').value.trim();
+
+        if(!emailInput || !passwordInput) {
+            alert('Por favor, preencha todos os campos. (email e senha)');
+            return;
+        }
+
+        const loginData = {
+            email: emailInput,
+            password: passwordInput
+        };
+
+        const API_URL = "http://localhost:5000";
+
+        try {
+            const response = await fetch(API_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(loginData)
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                
+                console.log('Login efetuado com sucesso!', data);
+                
+                if (data.token) {
+                    localStorage.setItem('authToken', data.token);
+                }
+                
+                alert('Login realizado com sucesso!');
+                window.location.href = '/dashboard.html';
+                
+            } else {
+                const errorData = await response.json();
+                alert(`Falha no login: ${errorData.message || 'Credenciais inválidas.'}`);
+            }
+        }catch(error){
+            console.error('Erro na requisição Fetch:', error);
+            alert('Ocorreu um erro de conexão com o servidor. Tente novamente mais tarde.');
+        }
+    })
+});
